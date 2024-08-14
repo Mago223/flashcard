016464740d13508fb4b837aeeac1b6d60a4d42c0
D-G Singla <dsingla340@gmail.com>
2024-08-14 17:42:57 -0400

    Finished base app

--- app/generate/page.js
+++ app/generate/page.js
@@ -1,17 +1,18 @@
 "use client"
-import { Box, Container, TextField, Typography, Paper, Button } from "@mui/material";
+import { Box, Container, TextField, Typography, Paper, Button, CardActionArea, CardContent, Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions, Grid, Card } from "@mui/material";
 import { useUser } from "@clerk/clerk-react";
 import { useState } from "react";
-import { writeBatch } from "firebase/firestore";
 import { useRouter } from "next/navigation";
+import { db } from "@/firebase";
+import { doc, collection, setDoc, getDoc, writeBatch } from "firebase/firestore";
 
 export default function Generate(){
     const {isLoaded, isSignedIn, user} = useUser();
-    const {flashcards, setFlashcards} = useState([]);
-    const {flipped, setFlipped} = useState([]);
+    const [flashcards, setFlashcards] = useState([]);
+    const [flipped, setFlipped] = useState([]);
     const [text, setText] = useState("");
-    const {name, setName} = useState("");
-    const {open, setOpen} = useState(false);
+    const [name, setName] = useState("");
+    const [open, setOpen] = useState(false);
     const router = useRouter();
 
     const handleSubmit = async () => {
@@ -20,7 +21,7 @@ export default function Generate(){
             body: text
         })
         .then((res)=>res.json())
-        .then((data)=>setFlashcards(data))
+        .then((data) => setFlashcards(data))
     }
 
     const handleCardClick = (id) => {
@@ -95,7 +96,84 @@ export default function Generate(){
         {flashcards.length > 0 && (
             <Box sx={{mt:4}}>
                 <Typography variant="h5">Flashcards Preview</Typography>
+                <Grid container spacing={3}>
+                    {flashcards.map((flashcard, index) => (
+                        <Grid item xs={12} sm={6} md={4} key={index}>
+                            <Card>
+                                <CardActionArea onClick={() => handleCardClick(index)}>
+                                    <CardContent>
+                                        <Box sx={{perspective: '1000px', 
+                                            '& > div':{
+                                                transition: 'transform 0.6s',
+                                                transformStyle: 'preserve-3d',
+                                                position: 'relative',
+                                                width: '100%',
+                                                height: '200px',
+                                                boxShadow: '0 4px 8px 0 rgba(0,0,0,0.2)',
+                                                transform: flipped[index]
+                                                ? 'rotateY(180deg)'
+                                                : 'rotateY(0deg)'
+                                            },
+                                            '& > div > div':{
+                                                position: 'absolute',
+                                                width: '100%',
+                                                height: '100%',
+                                                backfaceVisibility: 'hidden',
+                                                display: 'flex',
+                                                justifyContent: 'center',
+                                                alignItems: 'center',
+                                                padding: 2,
+                                                boxSizing: 'border-box'
+                                            },
+                                            '& > div > div:nth-of-type(2)':{
+                                                transform: 'rotateY(180deg)'
+                                            }
+                                        }}>
+                                            <div>
+                                                <div>
+                                                    <Typography variant="h5" component="div">
+                                                        {flashcard.front}
+                                                    </Typography>
+                                                </div>
+                                                <div>
+                                                    <Typography variant="h5" component="div">
+                                                        {flashcard.back}
+                                                    </Typography>
+                                                </div>
+                                            </div>
+                                        </Box>
+                                    </CardContent>
+                                </CardActionArea>
+                            </Card>
+                        </Grid>
+                    ))}
+                </Grid>
+                <Box sx={{mt: 4, display: 'flex', justifyContent:'center'}}>
+                    <Button variant="contained" color="secondary" onClick={handleOpen}>Save Flashcards</Button>
+                </Box>
             </Box>
         )}
+
+        <Dialog open={open} onClose={handleClose}>
+            <DialogTitle>Save Flashcards</DialogTitle>
+            <DialogContent>
+                <DialogContentText>
+                    Please enter a name for your flashcards collection
+                </DialogContentText>
+                <TextField
+                    autoFocus
+                    margin='dense'
+                    label='collection Name'
+                    type='text'
+                    fullWidth
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    variant="outlined"/>
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={handleClose}>Cancel</Button>
+                <Button onClick={saveFlashcards}>Save</Button>
+            </DialogActions>
+        </Dialog>
     </Container>
 }
--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+'use client'
 import Image from "next/image";
 import getStripe from "@/utils/get-stripe";
 import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
@@ -5,6 +6,29 @@ import { AppBar, Button, Container, Toolbar, Typography, Box, Grid } from "@mui/
 import Head from "next/head";
 
 export default function Home() {
+  const handleSubmit = async () => {
+    const checkoutSession = await fetch("/api/checkout_session",{
+      method: "POST",
+      headers: {
+        origin: 'http://localhost:3000',
+      },
+    })
+
+    const checkoutSessionJSON = await checkoutSession.json();
+
+    if(checkoutSession.statusCode === 500){
+      console.error(checkoutSessionJSON.message);
+    }
+
+    const stripe = await getStripe();
+    const {error} = await stripe.redirectToCheckout({
+      sessionId: checkoutSessionJSON.id,
+    });
+
+    if (error){
+      console.warn(error.message);
+    }
+  }
   return (
     <Container maxWidth="100vw">
       <Head>
@@ -31,33 +55,33 @@ export default function Home() {
         textAlign: "center",
         my: 4,
       }}>
-        <Typography variant="h2" gutterBottom>Welcome to Flashcards!</Typography>
-        <Typography variant="h5" gutterBottom>The easiest way to make flashcards from scratch</Typography>
+        <Typography variant="h2" gutterbottom>Welcome to Flashcards!</Typography>
+        <Typography variant="h5" gutterbottom>The easiest way to make flashcards from scratch</Typography>
         <Button variant="contained" color="primary" sx={{mt: 2}}>Get Started</Button>
       </Box>
       <Box sx={{my: 6}}>
-        <Typography variant="h4" gutterBottom>
+        <Typography variant="h4" gutterbottom>
           Features
         </Typography>
         <Grid container spacing={4}>
           <Grid item xs={12} md={4}>
-            <Typography variant="h6" gutterBottom>Easy text input</Typography>
+            <Typography variant="h6" gutterbottom>Easy text input</Typography>
             <Typography>Just type in your text and we'll do the rest</Typography>
           </Grid>
           <Grid item xs={12} md={4}>
-            <Typography variant="h6" gutterBottom>Smart Flashcards</Typography>
+            <Typography variant="h6" gutterbottom>Smart Flashcards</Typography>
             <Typography>Our AI intelligently breaks down your text into concise flashcards,
               perfect for studying
             </Typography>
           </Grid>
-          <Grid item xs={12} md={4} gutterBottom>
+          <Grid item xs={12} md={4} gutterbottom>
             <Typography variant="h6">Accessible Anywhere</Typography>
             <Typography>Access your flashcards from any device, at any time. Study on the go with ease.</Typography>
           </Grid>
         </Grid>
       </Box>
       <Box sx={{my: 6, textAlign: 'center'}}>
-        <Typography variant="h4" gutterBottom>Pricing</Typography>
+        <Typography variant="h4" gutterbottom>Pricing</Typography>
         <Grid container spacing={4}>
           <Grid item xs={12} md={6}>
             <Box sx={{
@@ -66,8 +90,8 @@ export default function Home() {
               borderColor: 'grey.300',
               borderRadius: 2,
             }}>
-              <Typography variant="h5" gutterBottom>Basic</Typography>
-              <Typography variant="h6" gutterBottom>$5 / month</Typography>
+              <Typography variant="h5" gutterbottom>Basic</Typography>
+              <Typography variant="h6" gutterbottom>$5 / month</Typography>
               <Typography>Access to basic flashcard features and limited storage</Typography>
               <Button variant="contained" color="primary" sx={{mt: 2}}>Choose Basic</Button>
             </Box>
@@ -79,10 +103,10 @@ export default function Home() {
               borderColor: 'grey.300',
               borderRadius: 2,
             }}>
-              <Typography variant="h5" gutterBottom>Pro</Typography>
-              <Typography variant="h6" gutterBottom>$10 / month</Typography>
+              <Typography variant="h5" gutterbottom>Pro</Typography>
+              <Typography variant="h6" gutterbottom>$10 / month</Typography>
               <Typography>Unlimited flashcards and storage with priority support</Typography>
-              <Button variant="contained" color="primary" sx={{mt: 2}}>Choose Pro</Button>
+              <Button variant="contained" color="primary" sx={{mt: 2}} onClick={handleSubmit}>Choose Pro</Button>
             </Box>
           </Grid>
         </Grid>
