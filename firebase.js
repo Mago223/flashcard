7396361e400f50f857bb2c905acb1265fe26ee29
Marcos Sanabria <sanabriam007@gmail.com>
2024-08-13 20:24:18 -0500

    added env variables

--- firebase.js
+++ firebase.js
@@ -6,13 +6,13 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA6nA53KeTVd7av63KibcQFKAdQlfHeGA4",
-  authDomain: "flashcard-550f9.firebaseapp.com",
-  projectId: "flashcard-550f9",
-  storageBucket: "flashcard-550f9.appspot.com",
-  messagingSenderId: "934859335262",
-  appId: "1:934859335262:web:61ce0c39184b5cbe9476cf",
-  measurementId: "G-863ECMDDTD",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
